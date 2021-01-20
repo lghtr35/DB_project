43c5fb6758c27e430efe2020ce989c6e7f38f881
Serdıl Çağın Çakmak <38701116+lghtr35@users.noreply.github.com>
2021-01-20 06:34:33 +0300

    db brought to local

--- DB_call.py
+++ DB_call.py
@@ -1,11 +1,16 @@
 import mysql.connector
 def get_db():
     
+    #db=mysql.connector.connect(
+    #        host="us-cdbr-east-03.cleardb.com",
+    #        user="b07da6fa923ec8",
+    #        password="53618f58",
+    #        database="heroku_9d68bac344849a5",
+    #        )
     db=mysql.connector.connect(
-            host="us-cdbr-east-03.cleardb.com",
-            user="b07da6fa923ec8",
-            password="53618f58",
-            database="heroku_9d68bac344849a5",
-            )
-        
+            host="localhost",
+            user="sckmk",
+            password="sanane22021999",
+            database="itu_social_app",
+            )  
     return db
